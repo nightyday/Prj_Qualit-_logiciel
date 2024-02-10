69f82d8168be8bf60378eaa1eaa802fd15b3c397
nightyday <benabboudouae@gmail.com>
2024-02-10 15:07:37 +0100

    menu user frame : done

---
+++ inventory/src/main/java/qualite_log/controller/MenuUserController.java
@@ -0,0 +1,75 @@
+package qualite_log.controller;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import qualite_log.session.SessionManager;
+
+public class MenuUserController {
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    private Parent root;
+
+    @FXML
+    private VBox menuVBox;
+
+    @FXML
+    void initialize() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/qualite_log/ToolTypeListFrame.fxml"));
+        root = fxmlLoader.load();
+        anchorPane.getChildren().add(root);
+    }
+
+    @FXML
+    public void handleMenuAction(ActionEvent event) {
+        Object source = event.getSource();
+        if (source instanceof MenuItem) {
+            MenuItem menuItem = (MenuItem) source;
+            String viewPath = getViewPathById(menuItem.getId());
+            changeView(viewPath);
+        }
+    }
+
+    private String getViewPathById(String id) {
+        switch (id) {
+            case "toolTypeListMenuItem":
+                return "/qualite_log/ToolTypeListFrame.fxml";
+            case "toolListMenuItem":
+                return "/qualite_log/ToolListFrame.fxml";
+            case "bookingCreateMenuItem":
+                return "/qualite_log/BookingCreateFrame.fxml";
+            case "bookingDeleteMenuItem":
+                return "/qualite_log/BookingDeleteFrame.fxml";
+
+            default:
+                return null;
+        }
+    }
+
+    private void changeView(String pathView) {
+        if (pathView != null) {
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(pathView));
+                root = fxmlLoader.load();
+                anchorPane.getChildren().clear();
+                anchorPane.getChildren().add(root);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @FXML
+    public void handleDeconnexion(ActionEvent event) {
+        SessionManager.clearSession();
+        changeView("/qualite_log/ConnexionFrame.fxml");
+    }
+}
